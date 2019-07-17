ae9514c7b92108736ba2af89c6d7b401f78a2aca
Xinran <52697166+xrma99@users.noreply.github.com>
2019-07-17 16:56:12 +0800

    Almost completed

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.onload = initLinkStyle;
+
+function initLinkStyle() {
+    
+    if (document.getElementsByClassName("guide")) {
+        var arrLink = document.getElementsByClassName("guide");
+        for (i = 0; i < arrLink.length; i++) {
+            var link = arrLink[i];
+            /*link.id = 'normal';*/
+            link.onclick = clickNav;
+            
+        }
+    }
+}
+
+
+
+/**
+ * 执行点击事件
+ * @param {Object} event 鼠标事件
+ */
+function clickNav(event) {
+    var target = event.currentTarget;
+
+    //擦除上次选择的a的样式
+    /*
+    if (document.getElementsByClassName("guide")) {
+        var arrLink = document.getElementsByClassName("guide");
+        for (i = 0; i < arrLink.length; i++) {
+            var link = arrLink[i];
+            
+            if (link.id == 'big-bold') {
+                link.id = 'normal';
+            }
+        }
+    }*/
+
+    target.id = 'big-bold';
+
+    return true;//阻止浏览器默认事件
+}
+
